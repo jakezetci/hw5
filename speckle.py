e3068242ef8bf300b488265caa7496d0a4f53705
jakezetci <cosb.off@gmail.com>
2022-05-12 22:26:13 +0300

    last one on thursday

--- speckle.py
+++ speckle.py
@@ -11,44 +11,53 @@ import matplotlib
 import matplotlib.pyplot as plt
 import astropy.io.fits as fits
 from scipy import ndimage
+from PIL import Image
+
 
 if __name__ == "__main__":
-
     data = fits.open('speckledata.fits')[2].data
     mean = np.mean(data, axis=0)
-    plt.figure()
-    a = plt.imsave('mean.png',mean)  
-    
 
-    fourier = np.abs(np.fft.fft(data, n=1, axis=0)[0])**2
+    plt.imsave('mean.png', mean, cmap='gray')
+
+
+    fourier = np.mean(np.abs(np.fft.fft2(data))**2, axis=0)
+    fourier = np.fft.fftshift(fourier)
 
 
     centre = np.unravel_index(np.argmax(fourier),
                               shape=fourier.shape)
-    r = 50  # да конкретно под задачу да говнокод
+    r = 50  # да конкретно под задачу
     fr_mask = np.full_like(fourier, fill_value=False, dtype=bool)
-    fr_mask[centre[0]-r:centre[0]+r, centre[1]-r:centre[1]+r] = True
-
+    for i in range(fourier.shape[0]):
+        for j in range(fourier.shape[1]):
+            if (i-100)**2+(j-100)**2 < r**2:
+                fr_mask[i, j] = True
     fr_masked = ma.MaskedArray(fourier, mask=fr_mask, dtype=np.float64)
     noise = fr_masked.mean()
     fourier = fourier - noise
-
+    plt.imsave('fourier.png',fourier,
+               vmax=np.quantile(np.mean(fourier), 0.98),
+                   cmap='gray')
     N = 360
     angle_mean = np.empty(N)
     rotated = np.empty((N, 200, 200))
     for i in range(N):
         rotatedim = ndimage.rotate(fourier, i * 360 / N, reshape=False)
-        angle_mean[i] = np.nanmean(np.mean(rotatedim, axis=0))
-        rotated[i] = ndimage.rotate(rotatedim/angle_mean[i], -i * 360 / N,
-                                    reshape=False)
+
+        rotated[i] =rotatedim
     im = np.mean(rotated, axis=0)
 
-    plt.figure()
-    b = plt.imsave('fourier.png',im,
+
+    plt.imsave('rotaver.png', im, vmax=np.quantile(np.mean(fourier), 0.98),
                    cmap='gray')
 
 
-    im_masked = np.atleast_3d(ma.MaskedArray(im, mask=fr_mask).filled(0))
-    im_new = np.fft.irfft(im_masked, n=101)
-    plt.figure()
-    plt.imshow(np.mean(im_new, axis=-1), cmap='gray')
+    im_masked = ma.MaskedArray(fourier/im, mask=np.invert(fr_mask)).filled(
+        fill_value=0)
+    im_new = abs(np.fft.ifft2((im_masked), axes=(0,1)))
+    im_new = np.fft.fftshift(im_new)
+    im_new = Image.fromarray(im_new).resize((512, 512))
+    plt.imsave('binary.png', im_new1,
+                cmap='gray')
+    
