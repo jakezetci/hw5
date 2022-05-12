da5649b2eb61c0d95f6de04c652f2dc7534dcc1e
jakezetci <cosb.off@gmail.com>
2022-05-12 22:50:22 +0300

    last one with bonus task

--- speckle.py
+++ speckle.py
@@ -7,11 +7,12 @@ Created on Fri Apr 29 11:55:34 2022
 
 import numpy as np
 import numpy.ma as ma
-import matplotlib
 import matplotlib.pyplot as plt
 import astropy.io.fits as fits
 from scipy import ndimage
 from PIL import Image
+from photutils.detection import find_peaks
+import json
 
 
 if __name__ == "__main__":
@@ -20,10 +21,8 @@ if __name__ == "__main__":
 
     plt.imsave('mean.png', mean, cmap='gray')
 
-
     fourier = np.mean(np.abs(np.fft.fft2(data))**2, axis=0)
     fourier = np.fft.fftshift(fourier)
-
 
     centre = np.unravel_index(np.argmax(fourier),
                               shape=fourier.shape)
@@ -36,28 +35,35 @@ if __name__ == "__main__":
     fr_masked = ma.MaskedArray(fourier, mask=fr_mask, dtype=np.float64)
     noise = fr_masked.mean()
     fourier = fourier - noise
-    plt.imsave('fourier.png',fourier,
+    plt.imsave('fourier.png', fourier,
                vmax=np.quantile(np.mean(fourier), 0.98),
-                   cmap='gray')
+               cmap='gray')
     N = 360
     angle_mean = np.empty(N)
     rotated = np.empty((N, 200, 200))
     for i in range(N):
         rotatedim = ndimage.rotate(fourier, i * 360 / N, reshape=False)
-
-        rotated[i] =rotatedim
+        rotated[i] = rotatedim
     im = np.mean(rotated, axis=0)
 
-
     plt.imsave('rotaver.png', im, vmax=np.quantile(np.mean(fourier), 0.98),
-                   cmap='gray')
-
+               cmap='gray')
 
     im_masked = ma.MaskedArray(fourier/im, mask=np.invert(fr_mask)).filled(
         fill_value=0)
-    im_new = abs(np.fft.ifft2((im_masked), axes=(0,1)))
-    im_new = np.fft.fftshift(im_new)
-    im_new = Image.fromarray(im_new).resize((512, 512))
-    plt.imsave('binary.png', im_new1,
-                cmap='gray')
-    
+    binary = abs(np.fft.ifft2((im_masked), axes=(0, 1)))
+    binary = np.fft.fftshift(binary)
+    binary2 = Image.fromarray(binary).resize((512, 512))
+    plt.imsave('binary.png', binary2,
+               cmap='gray')
+
+    '''бонуска'''
+    threshold = np.mean(binary) + (2*np.std(binary))
+    tbl = find_peaks(binary, threshold, box_size=11)
+    tbl['peak_value'].info.format = '%.8g'
+    peak_x = tbl['x_peak']
+    peak_y = tbl['y_peak']
+    dis = np.sqrt((peak_x[1]-peak_x[0])**2+(peak_y[1]-peak_y[0])**2)
+    dictionary = {"distance": dis*0.0206}
+    with open('binary.json', 'w') as f:
+        json.dump(dictionary, f)
